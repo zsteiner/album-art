4e9f4d794a4f9a1094dae279a4447e479f071387
Zach Steiner <zsteiner@gmail.com>
2018-12-18 10:27:27 -0700

    Fix linting

--- src/store.js
+++ src/store.js
@@ -1,20 +1,19 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import axios from "axios";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     albums: {},
+    country: 'us',
+    media: 'music',
     searchTerm: null,
-    country: "us",
-    media: "music"
+    type: 'album',
   },
   getters: {
-    getAppleAlbums: state => {
-      return state.albums;
-    }
+    getAppleAlbums: state => state.albums,
   },
   mutations: {
     getAppleAlbums(state, data) {
@@ -23,31 +22,32 @@ export default new Vuex.Store({
         artist: album.artistName,
         title: album.collectionName,
         cover: album.artworkUrl100,
-        coverMedRes: album.artworkUrl100.replace("100x100b", "600x600b"),
+        coverMedRes: album.artworkUrl100.replace('100x100b', '600x600b'),
         coverHighRes: album.artworkUrl100.replace(
-          "100x100b",
-          "100000x100000-999b"
-        )
+          '100x100b',
+          '100000x100000-999b',
+        ),
       }));
     },
     search(state, query) {
       state.searchTerm = query;
-    }
+    },
   },
   actions: {
-    getAppleAlbums({ commit, state }, { type }) {
+    getAppleAlbums({ commit, state }) {
       const encodedQuery = encodeURI(state.searchTerm);
       const api = `https://itunes.apple.com/search?term=${encodedQuery}&country=${
         state.country
-      }&media=${state.media}&entity=${type}`;
+      }&media=${state.media}&entity=${state.type}`;
+
       axios
         .get(api)
         .then(response => {
-          commit("getAppleAlbums", response.data.results);
+          commit('getAppleAlbums', response.data.results);
         })
         .catch(event => {
           console.error(event); //eslint-disable-line
         });
-    }
-  }
+    },
+  },
 });
